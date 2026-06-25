b29c79e213d2939384cfb4964b15e3247f3c8501
Daniel Lima <yourwebmaker@gmail.com>
2026-06-25 09:48:27 -0300

    Add fixtures with dev/test-only auto-load on container start

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -34,6 +34,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 		if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
 			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+			php bin/console app:load-fixtures-if-empty
 		fi
 	fi
 
